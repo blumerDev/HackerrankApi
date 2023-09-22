f63f4953c0073cdeafd6ff563f548cf798979398
Florencia <sicref001@gmail.com>
2023-09-22 14:22:10 -0300

    first commit

---
+++ src/main/java/com/example/Hackerrank/HackerrankApplication.java
@@ -0,0 +1,31 @@
+package com.example.Hackerrank;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import services.CalculateGoalService;
+
+@SpringBootApplication
+public class HackerrankApplication {
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HackerrankApplication.class, args);
+        final String COMPETITION = "UEFA Champions League";
+        int YEAR = 2011;
+
+        System.out.println("RTA FINAL: " + CalculateGoalService.getWinnerTotalGoals(COMPETITION, YEAR));
+
+    }
+
+}
